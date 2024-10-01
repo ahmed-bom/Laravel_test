d608fd3c3eb397225397cdf8e60b1d65834e5e1d
Ahmed <ahmed.boum.bn0gmail.com>
2024-10-01 22:38:42 +0200

    play wid components

--- opencollab/resources/views/about.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.app')
-@section('title',"about")
-@section('content')
-<h1>test héritage</h1>
-<h2>{{$company}}</h2>
-@endsection
--- opencollab/routes/web.php
+++ opencollab/routes/web.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/{name}', function ($name) {
-//     return view('welcome',['name'=>$name]);
-// });
-
-Route::get('/about', function () {
-    return view("about",['company'=>"ahmed_crl"]);
+Route::get('/', function () {
+    return view('login_test');
 });
+
+Route::get('/main', function () {
+    return view('main_test');
+});
+
